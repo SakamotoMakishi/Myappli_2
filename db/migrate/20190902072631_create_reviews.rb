0f0b8ee8ccd06c2977ef467b05b553cd956b9adc
SakamotoMakishi <dauw.mf81@gmail.com>
2019-09-02 16:35:13 +0900

    モデル、テーブル作成

---
+++ db/migrate/20190902072631_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :name
+      t.float :rate
+      t.text :review
+      t.integer :book_id
+      t.timestamps
+    end
+  end
+end
