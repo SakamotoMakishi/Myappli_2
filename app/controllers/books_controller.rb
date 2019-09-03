954fa669bdf6cf994768ea8eec9dfe8f0b62e563
SakamotoMakishi <dauw.mf81@gmail.com>
2019-09-03 18:40:15 +0900

    ランキング機能修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.order('created_at DESC').limit(3)
-    @ranking = Book.includes(:user).find(Review.group(:book_id).order('count(book_id) desc').limit(10).pluck(:Book_id))
+    @ranking = Book.includes(:user).find(Review.group(:book_id).order('count(book_id) desc').limit(10).pluck(:book_id))
     if user_signed_in?
       book_data
     end
